893893e9a5d4cdb7e6164c18b87091a76f4f4cf1
unknown <iron-goga@yandex.ru>
2020-10-03 00:36:50 +0300

    complete directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface Post {
-  title: string,
-  text: string,
-  id?: number
-};
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  posts: Post[] = [
-    {
-      title: 'Learn Angular components',
-      text: 'Still learning...',
-      id: 1,
-    },
-    {
-      title: 'Next block',
-      text: 'Directives',
-      id: 2,
-    },
-  ];
-
-  ngOnInit(): void {
-    // setTimeout( () => {
-    //   console.log('Timeout');
-    //   this.posts[0] = {
-    //     title: 'Changed',
-    //     text: 'Changed 2',
-    //     id: 48
-    //   };
-    // }, 3000);
-  };
-
-  updatePost(post: Post): void {
-    this.posts.unshift(post);
-  };
-
-  removePost(id: number): void{
-    this.posts = this.posts.filter( post => post.id !== id);
-  };
+  isVisible: boolean = true;
 
 }
