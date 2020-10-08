ff9960c5cc5749d4cf623b65713e29b2b9311a06
unknown <iron-goga@yandex.ru>
2020-10-08 20:01:53 +0300

    forms init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
 import { Component } from '@angular/core';
-import { AppCounterService } from './services/app-counter.service';
-import { MyCounterService } from './services/my-counter.service';
-
-export interface Post {
-  title: string,
-  text: string
-};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [MyCounterService]
 })
 export class AppComponent {
-  constructor(public myCounterService: MyCounterService){
-
-  };
-
 
 };
