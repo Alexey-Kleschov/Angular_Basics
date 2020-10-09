1e00e2a17d3e9392405a0c50fe5f8a81d415c885
NEPHILIM <37773537+Alexey-Kleschov@users.noreply.github.com>
2020-10-09 19:29:41 +0300

    routing init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Todo, TodosService } from './todos.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [TodosService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  todos: Todo[] = [];
-  todoTitle: string = '';
-  loading: boolean = false;
-  error: string = '';
 
-  constructor(private todosService: TodosService){};
-//'https://jsonplaceholder.typicode.com/todos/1'
-
-  ngOnInit(): void {
-    this.fetchTodos();
-  };
-
-  addTodo(): void {
-    if(!this.todoTitle.trim()){
-      return;
-    };
-
-    const newTodo: Todo = {
-      title: this.todoTitle,
-      completed: false
-    };
-
-    this.todosService.addTodo(newTodo)
-      .subscribe( res => {
-        this.todos.push(res);
-        this.todoTitle = '';
-      }
-    );
-  };
-
-  fetchTodos(): void {
-    this.loading = true;
-    this.todosService.fetchTodos()
-      .subscribe( res => {
-        this.todos = res;
-        this.loading = false;
-      },
-      err => {
-        this.error = err.message;
-      }
-    );
-  };
-
-  removeTodo(id: number): void {
-    this.todosService.removeTodo(id)
-      .subscribe( () => {
-        this.todos = this.todos.filter( todo => todo.id !== id);
-      });
-  };
-
-  completeTodo(id: number): void {
-    this.todosService.completeTodo(id)
-      .subscribe( res => {
-          this.todos.find( todo => todo.id === res.id).completed = true;
-        },
-        err => {
-          this.error = err.message;
-        }
-      );
-  };
 
 };
