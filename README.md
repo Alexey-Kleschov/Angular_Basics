d82230963b5c815f6097e9a7e7538f494c821f3b
unknown <iron-goga@yandex.ru>
2020-10-22 01:13:58 +0300

    init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularBasics
+# AngularUnitTests
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.2.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,10 @@
-import {
-  Component,
-  ComponentFactoryResolver,
-  ViewChild
-} from '@angular/core';
-import { Meta, Title } from '@angular/platform-browser';
-import { ModalComponent } from './modal/modal.component';
-import { RefDirective } from './ref.directive';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  // modal: boolean = false;
-
-  @ViewChild(RefDirective, { static: false }) refDir: RefDirective;
-
-  constructor(
-    private resolver: ComponentFactoryResolver,
-    private title: Title,
-    private meta: Meta
-
-  ){
-    const t = title.getTitle();
-    title.setTitle('HELOOOOOOOOOO!');
-
-    this.meta.addTags([
-      {name: 'Keywords', content: 'angular,google,appcomponents'},
-      {name: 'description', content: 'this is app component'},
-    ]);
-  };
-
-  showModal(){
-    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
-    this.refDir.containerRef.clear();
-    const component = this.refDir.containerRef.createComponent(modalFactory);
-    component.instance.title = 'HELLO';
-    component.instance.close.subscribe( () => {
-      this.refDir.containerRef.clear();
-    });
-  };
-
+  title = 'angular-unit-tests';
 }
-
