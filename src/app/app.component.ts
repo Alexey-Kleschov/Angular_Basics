42d22fe974fbb08b42f9b758296519295fe5cee2
unknown <iron-goga@yandex.ru>
2020-10-22 00:49:11 +0300

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import {Component} from '@angular/core'
+import {
+  Component,
+  ComponentFactoryResolver,
+  ViewChild
+} from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { ModalComponent } from './modal/modal.component';
+import { RefDirective } from './ref.directive';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +13,35 @@ import {Component} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+
+  // modal: boolean = false;
+
+  @ViewChild(RefDirective, { static: false }) refDir: RefDirective;
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+
+  ){
+    const t = title.getTitle();
+    title.setTitle('HELOOOOOOOOOO!');
+
+    this.meta.addTags([
+      {name: 'Keywords', content: 'angular,google,appcomponents'},
+      {name: 'description', content: 'this is app component'},
+    ]);
+  };
+
+  showModal(){
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+    this.refDir.containerRef.clear();
+    const component = this.refDir.containerRef.createComponent(modalFactory);
+    component.instance.title = 'HELLO';
+    component.instance.close.subscribe( () => {
+      this.refDir.containerRef.clear();
+    });
+  };
+
 }
 
