9d81771bef0bc01f399660613c531b1e328be5e5
unknown <iron-goga@yandex.ru>
2020-10-29 12:26:09 +0300

    anim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,11 +34,25 @@ import {
         })),
         animate(750)
       ]),
+      //void => *    :enter
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('850ms ease-out')
+      ]),
+      //* => void      :leave
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(750, style({
+          opacity: 0,
+          transform: 'scale(1.2)'
+        }))
+      ]),
     ])
   ]
 })
 export class AppComponent {
   boxState = 'start';
+  visible = true;
 
   animate() {
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
